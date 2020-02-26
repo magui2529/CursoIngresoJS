ead3addf250a9d29cdc1eab33e561abb874d53f7
magui2529 <60674738+magui2529@users.noreply.github.com>
2020-02-26 08:09:57 -0300

    26022020

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+var notas;
+var sexo;
+var cont=0;
+var promedio;
+var acumulador=0;
 
+do
+{
+notas=(parseInt(prompt("Ingrese nota del 0 al 10")));
+
+while(isNaN(notas) || notas<0 || notas>10)
+{
+    notas=prompt("Nota inválida. Ingrese nota entre 0 y 10");
+    
+}
+    sexo=prompt("Ingrese f o m indicando el sexo");
+    
+    while (sexo!="f" && sexo!="m")
+    {
+        
+        sexo=prompt("INVÁLIDO. Ingrese nuevamente el sexo.");
+
+    } 
+    cont++;
+    acumulador=acumulador+notas;
+ 
+    
+} while(cont<5);
+
+promedio=acumulador/cont;
+
+alert("El promedio es " + promedio);
 }
